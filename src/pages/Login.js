66ec2fa08660586e65ca57e3897e8fc780b20fb9
legendisdaname <monsefyoussef77@gmail.com>
2025-10-29 16:01:49 +0100

    Fix divider border color for white theme

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -262,7 +262,7 @@ function Login() {
               {/* Elegant Divider */}
               <div className="relative my-8">
                 <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t border-border/50" />
+                  <span className="w-full border-t border-gray-200" />
                 </div>
                 <div className="relative flex justify-center text-xs uppercase">
                   <span className="bg-white px-4 text-gray-500 font-semibold tracking-wider">
